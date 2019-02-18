5d5d163ad534602192697b6d4caa97e0f295a3b6
David Acosta <davidacosta25081@yahoo.com>
2019-02-18 04:52:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,13 @@ def self.new_from_filename(filename)
   song
 end
 
-
+def self.create_from_filename(filename)
+  info = filename.split(" - ")
+  song = self.create
+  song.name = info[1].chomp(".mp3")
+  song.artist_name = info[0]
+  song
+end
 
 
 
